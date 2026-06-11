873bcec9e6ec349cc1f0a7768d0f5c98a71643a1
Sharifa26 <ismailsheriff2001@gmail.com>
2026-06-11 22:34:56 +0530

    inverting a binary tree

---
+++ Course/S13-Binary tree/problems/15-Invert Binary Tree.js
@@ -0,0 +1,68 @@
+/**Invert Binary TreeInvert Binary Tree
+ * 
+ * 
+ * Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+        this.next = null;
+    }
+}
+
+
+let tree = new Node(4);
+
+tree.left = new Node(2);
+tree.right = new Node(7);
+
+tree.left.left = new Node(1);
+tree.left.right = new Node(3);
+
+tree.right.left = new Node(6);
+tree.right.right = new Node(9);
+
+var invertTree = function (root) {
+    if (root === null) {
+        return null;
+    }
+
+    const temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
+
+
+console.log(invertTree(tree));
